3a46bf4c621051036f55d1d62058ba9cfa63f000
Asger Hallas <ahl@asgerhallas.dk>
2020-10-01 11:08:34 +0200

    Add conditional JToken comparison

--- ShouldBeLike.Tests/ShouldBeLikeTests.cs
+++ ShouldBeLike.Tests/ShouldBeLikeTests.cs
@@ -4,33 +4,41 @@ using Xunit;
 
 namespace ShouldBeLike.Tests
 {
+    public class JTokenComparisonTests
+    {
+        public JTokenComparisonTests()
+        {
+            Extensions.DefaultTestingComparisonBuilder.WithCustomComparison(new JTokenComparison());
+        }
+
+        [Fact]
+        public void JObjectShouldBeLike()
+        {
+            new JObject
+            {
+                ["name"] = "Andreas"
+            }.ShouldBeLike(new JObject
+            {
+                ["name"] = "Andreas"
+            });
+        }
+
+        [Fact]
+        public void DifferentJObjectFails()
+        {
+            Assert.Throws<DeepEqualException>(() =>
+                new JObject
+                {
+                    ["firstname"] = "Andreas"
+                }.ShouldBeLike(new JObject
+                {
+                    ["lastname"] = "Andreas"
+                }));
+        }
+    }
+
     public class ShouldBeLikeTests
     {
-        //[Fact]
-        //public void JObjectShouldBeLike()
-        //{
-        //    new JObject
-        //    {
-        //        ["name"] = "Andreas"
-        //    }.ShouldBeLike(new JObject
-        //    {
-        //        ["name"] = "Andreas"
-        //    });
-        //}
-
-        //[Fact]
-        //public void DifferentJObjectFails()
-        //{
-        //    Assert.Throws<DeepEqualException>(() =>
-        //        new JObject
-        //        {
-        //            ["firstname"] = "Andreas"
-        //        }.ShouldBeLike(new JObject
-        //        {
-        //            ["lastname"] = "Andreas"
-        //        }));
-        //}
-
         [Fact]
         public void TupleShouldBeLike()
         {
--- ShouldBeLike/JTokenComparison.cs
+++ ShouldBeLike/JTokenComparison.cs
@@ -1,22 +1,26 @@
-﻿using System;
+﻿#if NEWTONSOFT
+using System;
+using DeepEqual;
+using Newtonsoft.Json.Linq;
 using ComparisonResult = DeepEqual.ComparisonResult;
 using IComparison = DeepEqual.IComparison;
 using IComparisonContext = DeepEqual.IComparisonContext;
 
 namespace ShouldBeLike
 {
-    //public class JTokenComparison : IComparison
-    //{
-    //    public bool CanCompare(Type type1, Type type2) => typeof(JToken).IsAssignableFrom(type1) && type1 == type2;
+    public class JTokenComparison : IComparison
+    {
+        public bool CanCompare(Type type1, Type type2) => typeof(JToken).IsAssignableFrom(type1) && type1 == type2;
 
-    //    public (ComparisonResult result, IComparisonContext context) Compare(IComparisonContext context, object value1, object value2)
-    //    {
-    //        var t1 = (JToken) value1;
-    //        var t2 = (JToken) value2;
+        public (ComparisonResult result, IComparisonContext context) Compare(IComparisonContext context, object value1, object value2)
+        {
+            var t1 = (JToken)value1;
+            var t2 = (JToken)value2;
 
-    //        if (JToken.DeepEquals(t1, t2)) return (ComparisonResult.Pass, context);
+            if (JToken.DeepEquals(t1, t2)) return (ComparisonResult.Pass, context);
 
-    //        return (ComparisonResult.Fail, context.AddDifference(t1, t2));
-    //    }
-    //}
+            return (ComparisonResult.Fail, context.AddDifference(new BasicDifference(context.Breadcrumb, t1, t2, "dunno yet")));
+        }
+    }
 }
+#endif
